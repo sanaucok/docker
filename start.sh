ebcc9eddec7aaeb35882160cf24c3bbfe4104b03
sanaucok <115146539+sanaucok@users.noreply.github.com>
2022-10-06 17:28:15 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+hhvm --mode server -vServer.Type=fastcgi -vServer.Port=9000&
+service nginx start
+
+
+composer install
+
+
+echo "App running on port 8080"
+
+
+tail -f /var/log/hhvm/error.log
